75d541d698c1eb5b0004eb8a6c04d0a75357123e
Ivo <82698375+thebosph@users.noreply.github.com>
2022-10-08 15:57:20 +0300

    feat(react-hook-form): setup React Hook form (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+import Form from "./components/form";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <Form />
     </div>
   );
 }
